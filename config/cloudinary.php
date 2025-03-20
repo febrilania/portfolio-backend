0f2e4ee1126b937fafab906f3c365f866049e623
febrilania <febrilanianurislami@gmail.com>
2025-03-20 14:51:58 +0700

    coba cloud5

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -4,5 +4,5 @@ return [
     'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
     'api_key'    => env('CLOUDINARY_API_KEY'),
     'api_secret' => env('CLOUDINARY_API_SECRET'),
-    'secure'     => true,
+    'url'        => env('CLOUDINARY_URL'),
 ];
